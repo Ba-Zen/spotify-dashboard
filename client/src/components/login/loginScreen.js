fed1f4db87afaa7b6ce851a286c7953e2ab38c96
Ba-Zen <bazenatlas@gmail.com>
2020-01-14 18:34:56 -0500

    link fix

--- client/src/components/login/loginScreen.js
+++ client/src/components/login/loginScreen.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-let login =
-  process.env.NODE_ENV === 'production'
-    ? 'https://myspotify-backend.herokuapp.com/login'
-    : 'http://localhost:8888/login';
 const LoginScreen = () => (
   <div>
-    <a href={login}> Login to Spotify.... </a>
+    <a href='https://myspotify-backend.herokuapp.com/login'>
+      {' '}
+      Login to Spotify....{' '}
+    </a>
   </div>
 );
 
