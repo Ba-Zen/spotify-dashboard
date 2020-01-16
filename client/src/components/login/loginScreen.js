0ec5825c3d0053a5c829dc9a0459b85219610f10
Ba-Zen <bazenatlas@gmail.com>
2020-01-16 16:07:17 -0500

    login screen styled, demo info added

--- client/src/components/login/loginScreen.js
+++ client/src/components/login/loginScreen.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import './loginScreen.styles.scss';
 
 const LoginScreen = () => {
   return (
-    <div>
-      {/* <a href='https://myspotify-backend.herokuapp.com/login'>
+    <div className='login-wrapper'>
+      <a
+        className='login-button'
+        href='https://myspotify-backend.herokuapp.com/login'
+      >
+        {' '}
+        Login to Spotify{' '}
+      </a>
+      {/* <a className='login-button' href='http://localhost:8888/login'>
         {' '}
         Login to Spotify{' '}
       </a> */}
-      <a href='http://localhost:8888/login'> Login to Spotify </a>
+      <div className='demo-login'>
+        <p>
+          If you don't have your own Spotify account feel free to sign in with
+          the details below!
+        </p>
+        <h2>Email: myspotifydemo@gmail.com</h2>
+        <h2>Password: demo1234</h2>
+      </div>
     </div>
   );
 };
